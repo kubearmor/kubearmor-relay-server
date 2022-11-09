acac3d1b830718aeaabeee5c0ccdacf884444d31
Achref ben saad <achref@accuknox.com>
2022-11-09 15:21:55 +0000

    add arm support in docker images
    Signed-off-by: Achref ben saad <achref@accuknox.com>

--- relay-server/build/push_img.sh
+++ relay-server/build/push_img.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+BASE_DIR=`dirname $(realpath "$0")`/../..
+cd $BASE_DIR
 [[ "$REPO" == "" ]] && REPO="kubearmor/kubearmor-relay-server"
+[[ "$PLATFORMS" == "" ]] && PLATFORMS="linux/amd64,linux/arm64/v8"
 
 # check version
 VERSION=latest
@@ -10,7 +13,7 @@ if [ ! -z $1 ]; then
 fi
 
 echo "[INFO] Pushing $REPO:$VERSION"
-docker push $REPO:$VERSION
+docker buildx build --platform $PLATFORMS --push -t $REPO:$VERSION -f $BASE_DIR/relay-server/Dockerfile .
 
 if [ $? != 0 ]; then
     echo "[FAILED] Failed to push $REPO:$VERSION"
