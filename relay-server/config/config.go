9aa77def2ffe69e9a9a728a72e662884ce3d9474
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2026-07-07 17:04:22 +0530

    chore: update health check service to query port 32766
    Signed-off-by: Aryan-sharma11 <aryan1126.sharma@gmail.com>

--- relay-server/config/config.go
+++ relay-server/config/config.go
@@ -8,6 +8,7 @@ import (
 
 type RelayConfig struct {
 	GRPC            string
+	LivenessPort    string
 	TLSEnabled      bool
 	TLSCertPath     string
 	TLSCertProvider string
@@ -17,6 +18,7 @@ var GlobalConfig RelayConfig
 
 const (
 	ConfigGRPC            string = "gRPCPort"
+	ConfigLivenessPort    string = "livenessPort"
 	ConfigTLSEnabled      string = "tlsEnabled"
 	ConfigTLSCertPath     string = "tlsCertPath"
 	ConfigTLSCertProvider string = "tlsCertProvider"
@@ -26,11 +28,13 @@ const (
 
 func readCmdLineParams() {
 	grpcStr := flag.String(ConfigGRPC, "32767", "gRPC port")
+	livenessPort := flag.String(ConfigLivenessPort, "32766", "liveness probe port")
 	tlsEnabled := flag.Bool(ConfigTLSEnabled, false, "enble tls to connect with kubearmor ssl service")
 	tlsCertPath := flag.String(ConfigTLSCertPath, "/var/lib/kubearmor/tls", "path to tls certs files ca.crt, client.crt, client.key")
 	tlsCertProvider := flag.String(ConfigTLSCertProvider, ExternalCertProvider, "source of certificate {self|external}, self: create certificate dynamically, external: provided by some external entity")
 	flag.Parse()
 	viper.SetDefault(ConfigGRPC, grpcStr)
+	viper.SetDefault(ConfigLivenessPort, livenessPort)
 	viper.SetDefault(ConfigTLSEnabled, tlsEnabled)
 	viper.SetDefault(ConfigTLSCertPath, tlsCertPath)
 	viper.SetDefault(ConfigTLSCertProvider, tlsCertProvider)
@@ -41,6 +45,7 @@ func LoadConfig() error {
 	readCmdLineParams()
 
 	GlobalConfig.GRPC = viper.GetString(ConfigGRPC)
+	GlobalConfig.LivenessPort = viper.GetString(ConfigLivenessPort)
 	GlobalConfig.TLSEnabled = viper.GetBool(ConfigTLSEnabled)
 	GlobalConfig.TLSCertPath = viper.GetString(ConfigTLSCertPath)
 	GlobalConfig.TLSCertProvider = viper.GetString(ConfigTLSCertProvider)
